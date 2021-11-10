eb5c42a9f83a5cab5e7aed330a270cb8c22f38a5
milaan1337 <fellnermilan@gmail.com>
2021-11-10 16:00:18 +0100

    menu szarsagok

--- core/src/csakennijottunk/Game/GameActors.java
+++ core/src/csakennijottunk/Game/GameActors.java
@@ -2,19 +2,3 @@ package csakennijottunk.Game;
 
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteStaticActor;
-
-public class GameActors extends OneSpriteStaticActor {
-    public GameActors(MyGame game) {
-        super(game, "green.png");
-    }
-}
-
-class PlayerActor extends OneSpriteStaticActor {
-    public PlayerActor(MyGame game){
-        super(game, "badlogic.jpg");
-        this.setWidth(50);
-        this.setHeight(50);
-        this.setX(200);
-        this.setY(200);
-    }
-}
