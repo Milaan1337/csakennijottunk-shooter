a5bd8c1f7152c022e15721012926914bd4279cbc
tuskeb <tuskebganz@gmail.com>
2021-11-30 15:42:00 +0100

    Stop

--- core/src/csakennijottunk/Game/BearActor.java
+++ core/src/csakennijottunk/Game/BearActor.java
@@ -1,5 +1,8 @@
 package csakennijottunk.Game;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteAnimatedActor;
 
@@ -7,7 +10,13 @@ public class BearActor extends OneSpriteAnimatedActor {
     public BearActor(MyGame game) {
         super(game, "bear.atlas");
         setFps(20);
-
+        addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                stop();
+            }
+        });
     }
 
     public void act(float delta){
