0531658f8d46150de9ddb643d57057c2c9103996
tuskeb <tuskebganz@gmail.com>
2021-11-30 15:29:05 +0100

    Betöltés

--- core/src/csakennijottunk/Menu/MenuGame.java
+++ core/src/csakennijottunk/Menu/MenuGame.java
@@ -1,6 +1,12 @@
 package csakennijottunk.Menu;
 
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import hu.csanyzeg.master.MyBaseClasses.Assets.AssetList;
+import hu.csanyzeg.master.MyBaseClasses.Assets.LoadingListener;
+import hu.csanyzeg.master.MyBaseClasses.Assets.LoadingStage;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
+import hu.csanyzeg.master.MyBaseClasses.Scene2D.ResponseViewport;
 
 public class MenuGame extends MyGame {
     public MenuGame(boolean debug) {
@@ -10,6 +16,7 @@ public class MenuGame extends MyGame {
     @Override
     public void create() {
         super.create();
+        setLoadingStage(new MyLoadingStage(this));
         setScreen(new MenuScreen(this));
 
     }
